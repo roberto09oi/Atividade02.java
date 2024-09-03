e32ef1a9d9f4afdbc9f7d9615ccd2dc2b98b9ba1
roberto09oi <soaresroberto394@gmail.com>
2024-09-02 21:04:10 -0300

    primeiro commit

---
+++ src/ClassificaçãoDeTemperatura.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ClassificaçãoDeTemperatura {
+    //pedir para o usuario inserir uma temperatura
+    //imprimir se for >30 QUENTE entre 15 e 30 AGRADAVEL <15 FRIO
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double temperatura = 0;
+
+        System.out.println("digite a temperatura: ");
+        temperatura = sc.nextDouble();
+
+        if (temperatura >30) {
+            System.out.println(" quentee!");
+        }
+        else if (temperatura >15 && temperatura <=30) {
+            System.out.println(" agradável");
+        }
+        else if (temperatura <15){
+            System.out.println("frio");
+        }
+    }
+
+}
